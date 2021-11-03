17258a03ee51d5e6c55d47afdb2f6aab611eab07
pourya ghazizadeh <pourya.gh095@gmail.com>
2021-11-03 23:05:36 +0100

    fix some bug

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,7 +1,7 @@
 const bars = document.querySelector("#bars");
 
 const menue = document.querySelector(".menue");
-
+const searchBox = document.querySelector(".searchbox-container");
 let barMood = false;
 bars.addEventListener("click", (e) => {
   barMood = !barMood;
@@ -10,4 +10,5 @@ bars.addEventListener("click", (e) => {
     : (bars.classList = "fal fa-times");
 
   barMood ? (menue.style.left = "-100%") : (menue.style.left = "0%");
+  barMood ? (searchBox.style.left = "-100%") : (searchBox.style.left = "0%");
 });
